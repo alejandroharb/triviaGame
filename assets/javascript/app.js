6d870a0d4c98dd4ccc198e34768d32627a3a2871
alejandroharb <alejandroharb@gmail.com>
2016-11-11 09:59:36 -0600

    troubleshooting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,36 +4,37 @@
     3. What's the largest organ in the human body? Skin
 */
 $(document).ready(function(){
-    var q1 = {
-                question: "How long is the human DNA?",
-                correct: "2 meters",
-                wrong1: "2 cm",
-                wrong2: "5 mm",
-                wrong3: "500cm",
-            }
     var questions = [
         {
             question: "Who invented Calculus?",
             correct: "Albert Einstein",
             wrong1: "Stephen Hawking",
             wrong2: "Thomas Edison",
-            wrong3: "Sir Issac Newton",
+            wrong3: "Sir Issac Newton"
         },
         {
             question: "What's the largest organ in the human body?",
-            correct: "heart",
-            wrong2: "skin",
-            wrong3: "lungs",
+            correct: "skin",
+            wrong1: "heart",
+            wrong2: "liver",
+            wrong3: "lungs"
         },
         {
             question: "How long is the human DNA?",
             correct: "2 meters",
             wrong1: "2 cm",
             wrong2: "5 mm",
-            wrong3: "500cm",
+            wrong3: "500cm"
+        },
+        {
+            question: "Roughly how many cells are in a human body?",
+            correct: "30 million",
+            wrong1: "200,000",
+            wrong2: "1 million",
+            wrong3: "300 trillion"
         }
     ]
-    console.log(questions[0].question);
+    console.log(questions[3].question);
 })
 
 
