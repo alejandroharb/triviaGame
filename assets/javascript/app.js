cde725a43b624afec72924232a13c63da1661aa4
alejandroharb <alejandroharb@gmail.com>
2016-11-10 18:32:34 -0600

    added all files,added trivia questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+/* Science Questions:
+    1. How long is the human DNA? 2 meters
+    2. Who invented Calculus? Issac Newton
+    3. What's the largest organ in the human body? Skin
+*/
+$(document).ready(function(){
+    var q1 = {
+        question: "How long is the human DNA?",
+        correct: "2 meters",
+        wrong1: "2 cm",
+        wrong2: "5 mm",
+        wrong3: "500cm",
+    }
+})
+
+
