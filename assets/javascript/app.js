080bb3c9a82b4fc84c22ddcc31216b5cde5eeb46
alejandro harb <alejandro harb>
2016-11-13 10:52:56 -0600

    click function edits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,10 +7,10 @@ $(document).ready(function(){
     var questions = [
         {
             question: "Who invented Calculus?",
-            correct: "Albert Einstein",
+            correct: "Sir Issac Newton",
             wrong1: "Stephen Hawking",
             wrong2: "Thomas Edison",
-            wrong3: "Sir Issac Newton"
+            wrong3: "Albert Einstein"
         },
         {
             question: "What's the largest organ in the human body?",
@@ -33,14 +33,18 @@ $(document).ready(function(){
             wrong2: "1 million",
             wrong3: "300 trillion"
         }
-    ]
-    console.log(questions[3].question);
+    ];
+    var i = 0;
     var time = 10;
     var interval;
-    var correct;
-    var incorrect;
-    console.log(questions[0].question);
-    for(var i = 0; i < questions.length; i++){
+    var correct = 0;
+    var incorrect = 0;
+
+    userChoice('#answer1',i);
+    userChoice('#answer2',i);
+    userChoice('#answer3',i);
+    userChoice('#answer4',i);
+    function questionDisplay(i) {
         $("#question").html("<h2>"+questions[i].question+ "</h2");
         $("#answer1").text(questions[i].correct);
         $("#answer2").text(questions[i].wrong1);
@@ -48,35 +52,40 @@ $(document).ready(function(){
         $("#answer4").text(questions[i].wrong3);
         //run timer
         setTimeout(next, 1000*10);
-        interval = setInterval(countDown,1000);
-        console.log(time)
-        //run questionCheck function
-        answerClicks;
-        // questionCheck(i)
-    }
-    //function for onclick of answers
-    var answerClicks = function(){
-        $('#answer1').on('click',function(){
-            answer = this;
-            functionCheck(i);
+        setInterval(countDown,1000);
+    };
+    questionDisplay(i);
+    console.log("global i: "+ i)    
+    
+    //detects user answers
+    function userChoice(id,i){
+        $(id).on('click',function(){
+            answer = $(this).text();
+            console.log(answer);
+            if(answer == questions[i].correct){
+                alert("Correct!");
+                correct++;
+                //run timer for few seconds, then run function for next question
+                setTimeout(next, 3000);
+            } else if(answer == questions[i].wrong1 || answer == questions[question].wrong2 || answer == questions[question].wrong3){
+                alert("Wrong!");
+                incorrect++;
+                setTimeout(next, 3000);
+            }
         })
-    }
+    };
     //once question displayed, checks questions or if time runs out
     var functionCheck = function(question) {
-        if(answer === questions[question].correct){
-            alert("Correct!");
-            correct++;
-            //run timer for few seconds, then run function for next question
-            setTimer()
-        }
+        
     }
     var countDown = function(){
         time--;
         $('#timer').text(time);
     }
-    console.log(countDown)
-    var next = function(){
-        return;
+    //adds 1 to i to run next round of question
+    var next = function(i){
+        i++;
+        questionDisplay(i);
     }
 
 })
