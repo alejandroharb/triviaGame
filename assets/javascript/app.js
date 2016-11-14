0d310a90a0f4d90a285b7c7723082b13b44c2216
alejandro harb <alejandro harb>
2016-11-13 22:19:04 -0600

    finished userAnswer answer check functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,7 @@
     2. Who invented Calculus? Issac Newton
     3. What's the largest organ in the human body? Skin
 */
+//to do: add objects to question, like time object, and..
 $(document).ready(function(){
     var questions = [
         {
@@ -10,77 +11,94 @@ $(document).ready(function(){
             correct: "Sir Issac Newton",
             wrong1: "Stephen Hawking",
             wrong2: "Thomas Edison",
-            wrong3: "Albert Einstein"
+            wrong3: "Albert Einstein",
+            time: 10
         },
         {
             question: "What's the largest organ in the human body?",
             correct: "skin",
             wrong1: "heart",
             wrong2: "liver",
-            wrong3: "lungs"
+            wrong3: "lungs",
+            time: 10
         },
         {
             question: "How long is the human DNA?",
             correct: "2 meters",
             wrong1: "2 cm",
             wrong2: "5 mm",
-            wrong3: "500cm"
+            wrong3: "500cm",
+            time: 10
         },
         {
             question: "Roughly how many cells are in a human body?",
             correct: "30 million",
             wrong1: "200,000",
             wrong2: "1 million",
-            wrong3: "300 trillion"
+            wrong3: "300 trillion",
+            time: 10
         }
     ];
-    console.log(questions[3].question);
-    var time = 10;
-    var interval;
-    var correct;
-    var incorrect;
-    console.log(questions[0].question);
-    //this is endless loop right now: fix
-    do{
-        var i = 0
+    var correct = 0;
+    var incorrect = 0;
+    var i = 0;
+    function startGame(startIndex){
         $("#question").html("<h2>"+questions[i].question+ "</h2");
         $("#answer1").text(questions[i].correct);
         $("#answer2").text(questions[i].wrong1);
         $("#answer3").text(questions[i].wrong2);
         $("#answer4").text(questions[i].wrong3);
-        //run timer
-        setTimeout(next, 1000*10);
-        interval = setInterval(countDown,1000);
-        console.log(time)
-        //run questionCheck function
-        answerClicks();
-        i++;
-    } while(i < questions.length)
-    //function for onclick of answers
-    function answerClicks(id){
-        $(id).on('click',function(){
-            answer = $(this).text();
-            console.log(answer);
-            functionCheck(i);
+        $("#answer1").on('click',function(){
+            displayQuestions(0);
         })
     }
-    answerClicks('#answer1');
-    answerClicks('#answer2');
-    answerClicks('#answer3');
-    answerClicks('#answer4');
+    startGame(0);
+    function displayQuestions(i){
+        $("#question").html("<h2>"+questions[i].question+ "</h2");
+        $("#answer1").text(questions[i].correct);
+        $("#answer2").text(questions[i].wrong1);
+        $("#answer3").text(questions[i].wrong2);
+        $("#answer4").text(questions[i].wrong3);
+    }
 
+    //function for onclick of answers
+    function userAnswer(id){
+        $(id).on('click',function(){
+            //displays questions depending on which question user is on.
+            displayQuestions(i);
+            //run timer
+            var count = setInterval(countDown,1000);
+            answer = $(this).text();
+            //checks answers if right or wrong
+            if(answer == questions[i].correct){
+                alert("Correct!");
+                correct++;
+                i++
+                displayQuestions(i);
+            }
+            if((answer == questions[i].wrong1) || (answer == questions[i].wrong2) || (answer == questions[i].wrong3)){
+                alert("incorrect!");
+                incorrect++;
+                i++
+                displayQuestions(i);
+            }
+
+        })
+    }
+    userAnswer('#answer1');
+    userAnswer('#answer2');
+    userAnswer('#answer3');
+    userAnswer('#answer4');
+    console.log("after click: "+i);
     //once question displayed, checks questions or if time runs out
-    var functionCheck = function(question) {
-        
-    }
+
     var countDown = function(){
-        time--;
+        var time = questions[i].time--;
         $('#timer').text(time);
-    }
-    //adds 1 to i to run next round of question
-    var next = function(i){
-        i++;
-        questionDisplay(i);
+        if(questions[i].time == 0){
+            i++
+            displayQuestions(i);
+        }
     }
 
 })
